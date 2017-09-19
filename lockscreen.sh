a11766316277c1471f97aad956f4d7fcf5b28349
Haden Collins <collinshaden@gmail.com>
2017-09-19 06:33:09 -0500

    Add locker

---
+++ lockscreen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# handle being called from systemd service
+if [ -z "$XDG_RUNTIME_DIR" ] && [ -z "$SWAYSOCK"]; then
+	uid=$(id -u $USER)
+	export XDG_RUNTIME_DIR="/run/user/"$uid"/"
+	export SWAYSOCK=$(find $XDG_RUNTIME_DIR -iname sway*sock)
+fi
+swaylock -i /home/haden/Downloads/lockscreen4.jpg
